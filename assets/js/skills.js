00f6b299f77823666832818663f6d2313ba36f76
Jaya <jaya5292.jmj@gmail.com>
2021-01-30 23:32:01 +0530

    jan 30 6

---
+++ assets/js/skills.js
@@ -0,0 +1,38 @@
+
+    [
+        {
+            "Skill": "Surprise awaiting...",
+            "Year": "2021"
+            
+        }, 
+        {
+            "Skill": "SAML SSO Implementation, SFDC REST API Integrations, SFDC B2B Commerce (CloudCraze), Handlebar JS,Heroku Architecture ",
+            "Year": "2020"
+          }, 
+        {
+            "Skill": "Lightning Web Components, Advanced JavaScript Events ",
+            "Year": "2019"
+          }, 
+      {
+        "Skill": "Salesforce Integrations, Apex, Angular Components, Node, Heroku ",
+        "Year": "2018"
+      },
+      {
+        "Skill": "Salesforce Development, Apex, Lightning Aura Component development",
+        "Year": "2017"
+      },
+      {
+        "Skill": "PHP, MySQL, AngularJS, Front-end Web Frameworks,Linux, DevOps, Cloud, Salesforce CRM Customizations",
+        "Year": "2016"
+      },
+      {
+        "Skill": "HTML5, CSS3 Animations, JavaScript",
+        "Year": "2015"
+      },
+      {
+        "Skill": "HTML,CSS",
+        "Year": "2014"
+      }
+
+    ]
+  
